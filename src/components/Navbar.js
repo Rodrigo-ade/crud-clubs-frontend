cdfd2a2345899c5f0fc543c826024d69a188ed84
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-04 20:34:36 -0300

    Add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <p>Navbar works!</p>
-    </div>
+    <nav className="navbar mb-3">
+      <div>
+          <ul className="navbar-nav flex-row">
+            <li className="nav-item text-center me-3">
+              <NavLink to="/">Clubs</NavLink>
+            </li>
+            <li className="nav-item ms-3">
+              <NavLink to="/form">Add Team</NavLink>
+            </li>
+          </ul>
+      </div>
+    </nav>
   );
 }
 
