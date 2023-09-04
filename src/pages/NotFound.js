2d87dcb701a7cde4b6ed085e2c7e8732808bde93
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-04 17:33:12 -0300

    Add page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <>
+      <h1>404</h1>
+      <h1>It looks the page you were looking for is missing</h1>
+    </>
+  );
+}
+
+export default NotFound;
