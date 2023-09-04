faad23574832621ecdbd7ac9669fea2c52e38487
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-04 20:54:30 -0300

    Refactor navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ function Navbar() {
   return (
     <nav className="navbar mb-3">
       <div>
-          <ul className="navbar-nav flex-row">
-            <li className="nav-item text-center me-3">
-              <NavLink to="/">Clubs</NavLink>
-            </li>
-            <li className="nav-item ms-3">
-              <NavLink to="/form">Add Team</NavLink>
-            </li>
-          </ul>
+        <ul className="navbar-nav flex-row">
+          <li className="nav-item text-center me-3">
+            <NavLink to="/">Clubs</NavLink>
+          </li>
+          <li className="nav-item ms-3">
+            <NavLink to="/form">Add Team</NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
