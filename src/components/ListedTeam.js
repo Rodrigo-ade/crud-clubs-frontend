9c8b13e753d8595fff11ca824681e2d83053c369
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-05 09:27:34 -0300

    Add listed team page

--- src/components/ListedTeam.js
+++ src/components/ListedTeam.js
@@ -2,9 +2,21 @@ import React from "react";
 
 function ListedTeam() {
   return (
-    <div className="listed-team">
-      <p>Listed Team works!</p>
-    </div>
+    <li className="list-group-item listed-club">
+      <div className="row">
+        <div className="col align-self-center">
+          <h3>Arsenal FC</h3>
+        </div>
+        <div className="col align-self-center">
+          <a href="/clubs/ARS"><button className="btn btn-sm btn-success m-1">Info</button></a>
+          <a href="/edit/clubs/ARS"><button className="btn btn-sm btn-info m-1">Edit</button></a>
+          <button className="btn btn-sm btn-warning m-1">Remove</button>
+        </div>
+        <div className="col align-self-center">
+          <img className="img-fluid listed-club-image" alt="Arsenal FC" src="https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg"></img>
+        </div>
+      </div>
+    </li>
   );
 }
 
--- src/pages/List.js
+++ src/pages/List.js
@@ -2,9 +2,15 @@ import ListedTeam from "../components/ListedTeam";
 
 function List() {
   return (
-    <div className="list">
-      <p>List works!</p>
-      <ListedTeam />
+    <div className="container text-center mt-1 mb-5">
+      <h1 className="mb-3"><strong>54</strong> clubs in database</h1>
+      <div className="row">
+        <div className="align-self-center">
+        <ul className="col-8 club-list list-group">
+            <ListedTeam />
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
