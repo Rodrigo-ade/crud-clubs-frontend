fa1d11756d63c8a5a809bfa54a31667cd3aca6a3
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-05 10:42:30 -0300

    Add form template
    action and method must be added when functionality is implemented

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -2,9 +2,78 @@ import React from "react";
 
 function Form() {
   return(
-    <div className="form">
-      <p>Form works!</p>
-    </div>
+    <form className="form-new-club mb-5 p-3" action="{{data.action}}" encType="multipart/form-data" method="{{data.method}}">
+      <h1 className="text-center">Add/Edit your club</h1>
+      <div style={{width:"70%", margin:"0 auto"}}>
+        <div className="mb-3">
+          <label htmlFor="clubName" className="form-label">Club name</label>
+          <input required type="text" className="form-control" id="clubName" name="name" placeholder="Boca Juniors" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="shortClubName" className="form-label">Short name</label>
+          <input required type="text" className="form-control" id="shortClubName" name="shortName" placeholder="Boca" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="tlaClubName" className="form-label">Three-letter acronym</label>
+          <input required type="text" className="form-control disabled" id="tlaClubName" name="tla" placeholder="BOC" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubLogo" className="form-label">Club Logo</label>
+          <input required className="form-control" type="file" id="clubLogo" name="logo_file"/>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubCountry" className="form-label">Country (location)</label>
+          <input required type="text" className="form-control" id="clubCountry" name="country" placeholder="Argentina" />
+        </div>
+        
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input required type="email" className="form-control" id="email" name="email" placeholder="myteam@example.com" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubAddress" className="form-label">Address</label>
+          <input required type="text" className="form-control" id="clubAddress" name="address" placeholder="Brandsen 805" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubWebsite" className="form-label">Website</label>
+          <input required type="text" className="form-control" id="clubWebsite" name="{{data.website}}" placeholder="www.bocajuniors.com" /> 
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubPhone" className="form-label">Phone number</label>
+          <input required type="text" className="form-control" id="clubPhone" name="phone" placeholder="45681234" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubFoundation" className="form-label">Foundation year</label>
+          <input required type="number" className="form-control" id="clubFoundation" name="founded" placeholder="1920" />
+        </div>
+
+        <div className="row">
+          <div className="col mb-3">
+            <label htmlFor="clubColor1" className="form-label">Color 1</label>
+            <input required type="text" className="form-control" id="clubColor1" name="color1" placeholder="Blue"/>
+          </div>
+          <div className="col mb-3">
+            <label htmlFor="clubColor2" className="form-label">Color 2</label>
+            <input required type="text" className="form-control" id="clubColor2" name="color2" placeholder="Yellow"/>
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="clubVenue" className="form-label">Venue</label>
+          <input required type="text" className="form-control" id="clubVenue" name="venue" placeholder="La Bombonera" />
+        </div>
+      </div>
+
+      <button className="btn btn-warning ms-5" type="submit">Add/Edit Club</button>
+    </form>
   );
 }
 
