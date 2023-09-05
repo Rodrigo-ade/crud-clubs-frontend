e5b9303ddabcd39092d2c4589c6f50a69f2b438b
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-05 15:53:20 -0300

    Add feature for "list" page
    Now it's possible to load and see clubs from database

--- src/components/ListedTeam.js
+++ src/components/ListedTeam.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-function ListedTeam() {
+function ListedTeam(props) {
   return (
     <li className="list-group-item listed-club">
       <div className="row">
         <div className="col align-self-center">
-          <h3>Arsenal FC</h3>
+          <h3>{props.name}</h3>
         </div>
         <div className="col align-self-center">
-          <a href="/clubs/ARS"><button className="btn btn-sm btn-success m-1">Info</button></a>
-          <a href="/edit/clubs/ARS"><button className="btn btn-sm btn-info m-1">Edit</button></a>
+          <a href={`/clubs/${props.tla}`}><button className="btn btn-sm btn-success m-1">Info</button></a>
+          <a href={`/edit/clubs/${props.tla}`}><button className="btn btn-sm btn-info m-1">Edit</button></a>
           <button className="btn btn-sm btn-warning m-1">Remove</button>
         </div>
         <div className="col align-self-center">
-          <img className="listed-club-image" alt="Arsenal FC" src="https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg"></img>
+          <img className="listed-club-image" alt={`${props.name} logo`} src={props.crestUrl}></img>
         </div>
       </div>
     </li>
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import ListedTeam from "../components/ListedTeam";
+import Loading from "../components/Loading";
+import { getClubs } from "../services/clubs";
 
 function List() {
-  return (
+  const [clubsList, setClubsList] = useState(null);
+
+  useEffect(() => {
+    getClubs().then((data) => {
+      setClubsList(data.map((club) => <ListedTeam name={club.name} crestUrl={club.crestUrl} tla={club.tla} key={club.id}/>));
+    });
+  },[]);
+
+  const list = (
     <div className="container text-center mt-1 mb-5">
-      <h1 className="mb-3"><strong>54</strong> clubs in database</h1>
+      <h1 className="mb-3"><strong>{clubsList && clubsList.length}</strong> clubs in database</h1>
       <div className="row">
         <div className="align-self-center">
-        <ul className="col-8 club-list list-group">
-            <ListedTeam />
+          <ul className="col-8 club-list list-group">
+            {clubsList}
           </ul>
         </div>
       </div>
     </div>
   );
+
+  return clubsList ? list : <Loading />;
 }
 
 export default List;
