c5e18a2a1c3bca856c070c09d49f643fd1ec72ce
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-06 13:09:19 -0300

    Fix pages and colors
    background color, success component, and notFound page are modified

--- src/components/Success.js
+++ src/components/Success.js
@@ -4,7 +4,7 @@ function Success() {
   return(
     <div className="success">
       <h1 className="text-center mt-5">Your request has been successfully completed!</h1>
-      <div class="text-center">
+      <div className="text-center">
         <a href="/"><button className="btn btn-outline-warning mt-5 btn-lg">Go back to teams</button></a>
       </div>
     </div>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,8 @@ function NotFound() {
   return (
     <>
       <h1 className="text-center mt-5">Oops!</h1>
-      <h1 className="text-center">404 - Not found!</h1>
+      <h2 className="text-center">something went wrong and we couldn't process your request.</h2>
+      <h5 className="text-center">try again later.</h5>
     </>
   );
 }
